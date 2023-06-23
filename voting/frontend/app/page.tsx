c9551c01df742239c042bd7c4b455ae0cb0a5649
Arnaud Sene <arnaudsene@MacBook-Pro-de-Arnaud.local>
2023-06-23 00:52:03 -0400

    feat: Init project
    - Init project with
      - NEXT.js
      - Hardhat
      - RainbowKit
      - Wagmi
    - Setup RainbowKit with first button
    - Add Voting.sol Smart Contract + deploy script

---
+++ voting/frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Voting project 3</h1>
+        <ConnectButton />
+    </main>
+  )
+}
