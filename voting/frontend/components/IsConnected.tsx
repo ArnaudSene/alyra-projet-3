da21d68468ae9d418bcfd8ab1e2041f93b6dacbf
Beater420 <caliaro.alexandre@gmail.com>
2023-06-29 01:28:40 +0200

    fix: minor fixes

--- voting/frontend/components/IsConnected.tsx
+++ voting/frontend/components/IsConnected.tsx
@@ -5,9 +5,7 @@ const IsConnected = ({ children }: any) => {
 
     return (
         <>
-            {isConnected ? (
-                <>{children}</>
-            ) : (
+            {isConnected ? children : (
                 <div className="mx-auto w-3/4 rounded h-auto min-h-[50px] text-center bg-gradient-to-r from-indigo-950 to-rose-600 text-zinc-200 shadow-lg">
                     <div className="p-6 font-semibold text-md">
                         Please connect your Wallet.
